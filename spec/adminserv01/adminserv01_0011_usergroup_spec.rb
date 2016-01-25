9961893c48750fa912052ff5d51cb7bdde9afa4a
Hideki Uemura <h-uemura@cs-fact.com>
2016-01-26 02:31:51 +0900

    実行されるSPECの説明を出力するように変更 (SPEC実行時の1行目についか)

--- spec/adminserv01/adminserv01_0011_usergroup_spec.rb
+++ spec/adminserv01/adminserv01_0011_usergroup_spec.rb
@@ -1,104 +1,107 @@
 require 'spec_helper'
 
+describe user('==================== USERチェック ====================') do
+
 #=====================================================================
 # infraグループ存在確認
 # GIDの確認
 #=====================================================================
-describe group('infra') do
-  it { should exist }
-  it { should have_gid 1400 }
-end
+  describe group('infra') do
+    it { should exist }
+    it { should have_gid 1400 }
+  end
 
 #=====================================================================
 # インフラユーザー存在確認
 # プライマリグループ存在確認
 # rootユーザーに属する
 #=====================================================================
-describe user('infra') do
-  it { should exist }
-  it { should belong_to_primary_group 'infra' }
-  it { should have_uid 1400 }
-  it { should have_home_directory '/home/infra' }
-  its(:encrypted_password) { should match('$6$infrasolt$a2vFx7QL/cV2Nplcy1AxeRp8Ss34MPTjkbKpu7au.kq.PXshj8K177TUH9Pj5wtG8Y83Ea5Nv8UlvRuBMN5Fu0') }
+  describe user('infra') do
+    it { should exist }
+    it { should belong_to_primary_group 'infra' }
+    it { should have_uid 1400 }
+    it { should have_home_directory '/home/infra' }
+    its(:encrypted_password) { should match('$6$infrasolt$a2vFx7QL/cV2Nplcy1AxeRp8Ss34MPTjkbKpu7au.kq.PXshj8K177TUH9Pj5wtG8Y83Ea5Nv8UlvRuBMN5Fu0') }
 
-end
+  end
 #=====================================================================
 # apacheグループ存在確認
 # GIDの確認
 #=====================================================================
-describe group('apache') do
-  it { should exist }
-  it { should have_gid 1100 }
-end
+  describe group('apache') do
+    it { should exist }
+    it { should have_gid 1100 }
+  end
 #=====================================================================
 # apacheユーザー存在確認
 # プライマリグループ存在確認
 #=====================================================================
-describe user('apache') do
-  it { should exist }
-  it { should belong_to_primary_group 'apache' }
-  it { should have_uid 1100 }
-  it { should have_home_directory '/home/apache' }
-  its(:encrypted_password) { should match('$6$apachesolt$PnTyYlBO7ZAmPmXNBxAgWj03udlCNxqOqWvfik.fM3A84UTG.Un0cWUaXEl2vT2Acme.lMsPVRapEOZ9MhWYr0') }
-end
+  describe user('apache') do
+    it { should exist }
+    it { should belong_to_primary_group 'apache' }
+    it { should have_uid 1100 }
+    it { should have_home_directory '/home/apache' }
+    its(:encrypted_password) { should match('$6$apachesolt$PnTyYlBO7ZAmPmXNBxAgWj03udlCNxqOqWvfik.fM3A84UTG.Un0cWUaXEl2vT2Acme.lMsPVRapEOZ9MhWYr0') }
+  end
 #=====================================================================
 # tomcatグループ存在確認
 # GIDの確認
 #=====================================================================
-describe group('tomcat') do
-  it { should exist }
-  it { should have_gid 1200 }
-end
+  describe group('tomcat') do
+    it { should exist }
+    it { should have_gid 1200 }
+  end
 #=====================================================================
 # tomcatユーザー存在確認
 # プライマリグループ存在確認
 #=====================================================================
-describe user('tomcat') do
-  it { should exist }
-  it { should belong_to_primary_group 'tomcat' }
-  it { should have_uid 1200 }
-  it { should have_home_directory '/home/tomcat' }
-  its(:encrypted_password) { should match('$6$tomcatsolt$Dz8QThSePXH5WZ0EjLQbUA8pZJrgIl4.2qsflSWkn1gV.RqD84vPT0.tdThOEn1VyowWJBsTgm2rmv5oJixDJ1') }
-end
+  describe user('tomcat') do
+    it { should exist }
+    it { should belong_to_primary_group 'tomcat' }
+    it { should have_uid 1200 }
+    it { should have_home_directory '/home/tomcat' }
+    its(:encrypted_password) { should match('$6$tomcatsolt$Dz8QThSePXH5WZ0EjLQbUA8pZJrgIl4.2qsflSWkn1gV.RqD84vPT0.tdThOEn1VyowWJBsTgm2rmv5oJixDJ1') }
+  end
 
 #=====================================================================
 # mysqlグループ存在確認
 # GIDの確認
 #=====================================================================
-describe group('mysql') do
-  it { should exist }
-  it { should have_gid 1300 }
-end
+  describe group('mysql') do
+    it { should exist }
+    it { should have_gid 1300 }
+  end
 
 #=====================================================================
 # mysqlユーザー存在確認
 # プライマリグループ存在確認
 #=====================================================================
-describe user('mysql') do
-  it { should exist }
-  it { should belong_to_primary_group 'mysql' }
-  it { should have_uid 1300 }
-  it { should have_home_directory '/home/mysql' }
-  its(:encrypted_password) { should match('$6$mysqlsolt$IeAbOR9NxGYA3Os9p43DRjdGvuTae62i707gWa2/XP/IFVyjHIYsEe/gH5uc15pxwyhAk8kGuRbi6Iinga5wh/') }
-end
+  describe user('mysql') do
+    it { should exist }
+    it { should belong_to_primary_group 'mysql' }
+    it { should have_uid 1300 }
+    it { should have_home_directory '/home/mysql' }
+    its(:encrypted_password) { should match('$6$mysqlsolt$IeAbOR9NxGYA3Os9p43DRjdGvuTae62i707gWa2/XP/IFVyjHIYsEe/gH5uc15pxwyhAk8kGuRbi6Iinga5wh/') }
+  end
 
 #=====================================================================
 # seleniumグループ存在確認
 # GIDの確認
 #=====================================================================
-describe group('selenium') do
-  it { should exist }
-  it { should have_gid 1600 }
-end
+  describe group('selenium') do
+    it { should exist }
+    it { should have_gid 1600 }
+  end
 
 #=====================================================================
 # seleniumユーザー存在確認
 # プライマリグループ存在確認
 #=====================================================================
-describe user('selenium') do
-  it { should exist }
-  it { should belong_to_primary_group 'selenium' }
-  it { should have_uid 1600 }
-  it { should have_home_directory '/home/selenium' }
-  its(:encrypted_password) { should match('$6$seleniumsolt$fl7ibc.Xf/.xnbIFqIXuGA3rtddYdZiUILqVvJO06cjGXy.HmpDh1LjXZYLKqzZymjn1GqmQkz2ndc8lfbfTz/') }
+  describe user('selenium') do
+    it { should exist }
+    it { should belong_to_primary_group 'selenium' }
+    it { should have_uid 1600 }
+    it { should have_home_directory '/home/selenium' }
+    its(:encrypted_password) { should match('$6$seleniumsolt$fl7ibc.Xf/.xnbIFqIXuGA3rtddYdZiUILqVvJO06cjGXy.HmpDh1LjXZYLKqzZymjn1GqmQkz2ndc8lfbfTz/') }
+  end
 end
--- spec/apserv01/apserv01_0011_usergroup_spec.rb
+++ spec/apserv01/apserv01_0011_usergroup_spec.rb
@@ -1,66 +1,67 @@
 require 'spec_helper'
+describe user('==================== USERチェック ====================') do
 
 #=====================================================================
 # infraグループ存在確認
 # GIDの確認
 #=====================================================================
-describe group('infra') do
-  it { should exist }
-  it { should have_gid 1400 }
-end
+  describe group('infra') do
+    it { should exist }
+    it { should have_gid 1400 }
+  end
 
 #=====================================================================
 # インフラユーザー存在確認
 # プライマリグループ存在確認
 # rootユーザーに属する
 #=====================================================================
-describe user('infra') do
-  it { should exist }
-  it { should belong_to_primary_group 'infra' }
-  it { should have_uid 1400 }
-  it { should have_home_directory '/home/infra' }
-  its(:encrypted_password) { should match('$6$infrasolt$a2vFx7QL/cV2Nplcy1AxeRp8Ss34MPTjkbKpu7au.kq.PXshj8K177TUH9Pj5wtG8Y83Ea5Nv8UlvRuBMN5Fu0') }
+  describe user('infra') do
+    it { should exist }
+    it { should belong_to_primary_group 'infra' }
+    it { should have_uid 1400 }
+    it { should have_home_directory '/home/infra' }
+    its(:encrypted_password) { should match('$6$infrasolt$a2vFx7QL/cV2Nplcy1AxeRp8Ss34MPTjkbKpu7au.kq.PXshj8K177TUH9Pj5wtG8Y83Ea5Nv8UlvRuBMN5Fu0') }
 
-end
+  end
 
 #=====================================================================
 # apacheグループ存在確認
 # GIDの確認
 #=====================================================================
-describe group('apache') do
-  it { should exist }
-  it { should have_gid 1100 }
-end
+  describe group('apache') do
+    it { should exist }
+    it { should have_gid 1100 }
+  end
 #=====================================================================
 # apacheユーザー存在確認
 # プライマリグループ存在確認
 #=====================================================================
-describe user('apache') do
-  it { should exist }
-  it { should belong_to_primary_group 'apache' }
-  it { should have_uid 1100 }
-  it { should have_home_directory '/home/apache' }
-  its(:encrypted_password) { should match('$6$apachesolt$PnTyYlBO7ZAmPmXNBxAgWj03udlCNxqOqWvfik.fM3A84UTG.Un0cWUaXEl2vT2Acme.lMsPVRapEOZ9MhWYr0') }
-end
+  describe user('apache') do
+    it { should exist }
+    it { should belong_to_primary_group 'apache' }
+    it { should have_uid 1100 }
+    it { should have_home_directory '/home/apache' }
+    its(:encrypted_password) { should match('$6$apachesolt$PnTyYlBO7ZAmPmXNBxAgWj03udlCNxqOqWvfik.fM3A84UTG.Un0cWUaXEl2vT2Acme.lMsPVRapEOZ9MhWYr0') }
+  end
 #=====================================================================
 # tomcatグループ存在確認
 # GIDの確認
 #=====================================================================
-describe group('tomcat') do
-  it { should exist }
-  it { should have_gid 1200 }
-end
+  describe group('tomcat') do
+    it { should exist }
+    it { should have_gid 1200 }
+  end
 
 #=====================================================================
 # tomcatユーザー存在確認
 # プライマリグループ存在確認
 #=====================================================================
-describe user('tomcat') do
-  it { should exist }
-  it { should belong_to_primary_group 'tomcat' }
-  it { should have_uid 1200 }
-  it { should have_home_directory '/home/tomcat' }
-  its(:encrypted_password) { should match('$6$tomcatsolt$Dz8QThSePXH5WZ0EjLQbUA8pZJrgIl4.2qsflSWkn1gV.RqD84vPT0.tdThOEn1VyowWJBsTgm2rmv5oJixDJ1') }
+  describe user('tomcat') do
+    it { should exist }
+    it { should belong_to_primary_group 'tomcat' }
+    it { should have_uid 1200 }
+    it { should have_home_directory '/home/tomcat' }
+    its(:encrypted_password) { should match('$6$tomcatsolt$Dz8QThSePXH5WZ0EjLQbUA8pZJrgIl4.2qsflSWkn1gV.RqD84vPT0.tdThOEn1VyowWJBsTgm2rmv5oJixDJ1') }
+  end
 end
-
 
