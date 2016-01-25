6b1e2f2b22362f3838b1828060850c0fed889fd5
Hideki Uemura <h-uemura@cs-fact.com>
2016-01-26 02:32:59 +0900

    ・yumにてインストールされるパッケージのインストール確認実装完了 ・Rakefileを一時的にyumパッケージしかテストしないように変更

--- spec/adminserv01/adminserv01_0012_yum_spec.rb
+++ spec/adminserv01/adminserv01_0012_yum_spec.rb
@@ -1,1 +1,86 @@
 require 'spec_helper'
+
+describe ('================ YUM インストール確認 ================') do
+  describe package('vim-enhanced') do
+    it { should be_installed }
+  end
+  describe package('openssl') do
+    it { should be_installed }
+  end
+  describe package('httpd') do
+    it { should be_installed }
+  end
+  describe package('wget') do
+    it { should be_installed }
+  end
+  describe package('unzip') do
+    it { should be_installed }
+  end
+  describe package('gcc') do
+    it { should be_installed }
+  end
+  describe package('git') do
+    it { should be_installed }
+  end
+  describe package('libffi-devel') do
+    it { should be_installed }
+  end
+  describe package('bind-utils') do
+    it { should be_installed }
+  end
+  describe package('net-tools') do
+    it { should be_installed }
+  end
+  describe package('mod_ssl') do
+    it { should be_installed }
+  end
+  describe package('man-pages-ja') do
+    it { should be_installed }
+  end
+  describe package('chrony') do
+    it { should be_installed }
+  end
+  describe package('openssl-devel') do
+    it { should be_installed }
+  end
+  describe package('readline-devel') do
+    it { should be_installed }
+  end
+  describe package('zlib-devel') do
+    it { should be_installed }
+  end
+  describe package('psmisc') do
+    it { should be_installed }
+  end
+  describe package('nfs-utils') do
+    it { should be_installed }
+  end
+  describe package('mlocate') do
+    it { should be_installed }
+  end
+  describe package('mariadb') do
+    it { should be_installed }
+  end
+  describe package('mariadb-server') do
+    it { should be_installed }
+  end
+  describe package('epel-release') do
+    it { should be_installed }
+  end
+  describe package('openvpn') do
+    it { should be_installed }
+  end
+  describe package('expect') do
+    it { should be_installed }
+  end
+  describe package('libselinux-python') do
+    it { should be_installed }
+  end
+  describe package('easy-rsa') do
+    it { should be_installed }
+  end
+  describe package('mod_dav_svn') do
+    it { should be_installed }
+  end
+
+end
--- spec/dbserv01/dbserv01_0012_yum_spec.rb
+++ spec/dbserv01/dbserv01_0012_yum_spec.rb
@@ -1,6 +1,72 @@
 require 'spec_helper'
 
+describe ('================ YUM インストール確認 ================') do
+  describe package('vim-enhanced') do
+    it { should be_installed }
+  end
+  describe package('openssl') do
+    it { should be_installed }
+  end
 
-describe package('httpd') do
-  it { should be_installed }
+  describe package('wget') do
+    it { should be_installed }
+  end
+  describe package('unzip') do
+    it { should be_installed }
+  end
+  describe package('gcc') do
+    it { should be_installed }
+  end
+  describe package('git') do
+    it { should be_installed }
+  end
+  describe package('libffi-devel') do
+    it { should be_installed }
+  end
+  describe package('bind-utils') do
+    it { should be_installed }
+  end
+  describe package('net-tools') do
+    it { should be_installed }
+  end
+
+  describe package('man-pages-ja') do
+    it { should be_installed }
+  end
+  describe package('chrony') do
+    it { should be_installed }
+  end
+  describe package('openssl-devel') do
+    it { should be_installed }
+  end
+  describe package('readline-devel') do
+    it { should be_installed }
+  end
+  describe package('zlib-devel') do
+    it { should be_installed }
+  end
+  describe package('psmisc') do
+    it { should be_installed }
+  end
+  describe package('nfs-utils') do
+    it { should be_installed }
+  end
+  describe package('mlocate') do
+    it { should be_installed }
+  end
+  describe package('mariadb') do
+    it { should be_installed }
+  end
+  describe package('mariadb-server') do
+    it { should be_installed }
+  end
+
+  describe package('expect') do
+    it { should be_installed }
+  end
+  describe package('libselinux-python') do
+    it { should be_installed }
+  end
+
+
 end
