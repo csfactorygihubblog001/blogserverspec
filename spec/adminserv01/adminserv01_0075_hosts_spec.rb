4cea208077687be01df6c43ca9d5eb62dd390d89
Hideki Uemura <h-uemura@cs-fact.com>
2016-01-27 02:31:39 +0900

    初期ディレクトリの作成とパーミッション設定

--- spec/adminserv01/adminserv01_0075_hosts_spec.rb
+++ spec/adminserv01/adminserv01_0075_hosts_spec.rb
@@ -1,1 +1,26 @@
 require 'spec_helper'
+describe "============= hostsで名前解決できるかpingが通るか ============= " do
+  describe host('manageterm') do
+    it { should be_resolvable.by('hosts')}
+    it { should be_reachable}
+    its(:ipaddress){should eq '192.168.134.195'}
+  end
+
+  describe host('adminserv01') do
+    it { should be_resolvable.by('hosts')}
+    it { should be_reachable}
+    its(:ipaddress){should match /127.0.0.1/}
+    its(:ipaddress){should match /192.168.134.198/}
+  end
+  describe host('apserv01') do
+    it { should be_resolvable.by('hosts')}
+    it { should be_reachable}
+    its(:ipaddress){should eq '192.168.134.196'}
+  end
+  describe host('dbserv01') do
+    it { should be_resolvable.by('hosts')}
+    it { should be_reachable}
+    its(:ipaddress){should eq '192.168.134.197'}
+  end
+
+end
