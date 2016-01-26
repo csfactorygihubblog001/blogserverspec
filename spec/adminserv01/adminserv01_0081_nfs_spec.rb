8749105483e7d3983077ac8f7e14acc3852e2d9b
Hideki Uemura <h-uemura@cs-fact.com>
2016-01-27 02:40:24 +0900

    NFSマウント確認

--- spec/adminserv01/adminserv01_0081_nfs_spec.rb
+++ spec/adminserv01/adminserv01_0081_nfs_spec.rb
@@ -1,1 +1,8 @@
 require 'spec_helper'
+
+describe "=========== NFS ==============" do
+  describe file ('/etc/exports') do
+    it { should contain '/var/opt/data/nfs 192.168.134.0/255.255.255.0(rw,no_root_squash)'}
+  end
+
+end
--- spec/dbserv01/dbserv01_0081_nfs_spec.rb
+++ spec/dbserv01/dbserv01_0081_nfs_spec.rb
@@ -1,1 +1,7 @@
 require 'spec_helper'
+describe " ==================== " do
+
+  describe file('/var/ot/data/nfs') do
+    it { be_mounted.with(:type => 'nfs')}
+  end
+end
