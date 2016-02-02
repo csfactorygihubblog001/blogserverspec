e6d136b89ce7d76c8768d0d28779830d33096ba6
hideki-uemura <h-uemura@cs-fact.com>
2016-02-03 00:32:28 +0900

    Firewallの確認（途中）

--- spec/adminserv01/adminserv01_3001_firewalld_spec.rb
+++ spec/adminserv01/adminserv01_3001_firewalld_spec.rb
@@ -1,1 +1,57 @@
 require 'spec_helper'
+
+
+describe 'firewalldの設定' do
+  describe service('firewalld') do
+    it {should be_enabled}
+    it {should be_running}
+  end
+
+  describe package('firewalld') do
+    it {should be_installed}
+  end
+
+  describe port(80) do
+    it {should be_listening}
+  end
+
+  describe command('firewall-cmd --list-interface --zone=trusted --permanent') do
+    its(:stdout){should match /tun0/}
+    its(:stdout){should match /lo/}
+  end
+  describe command('firewall-cmd --list-interface --zone=trusted') do
+    its(:stdout){should match /tun0/}
+    its(:stdout){should match /lo/}
+  end
+
+  describe command('firewall-cmd  --zone=trusted --query-masquerade --permanent') do
+    its(:stdout){should match /^yes/}
+  end
+
+  describe command('firewall-cmd  --zone=trusted --query-masquerade') do
+    its(:stdout){should match /^yes/}
+  end
+
+  describe command('firewall-cmd --list-interface --zone=dmz --permanent') do
+    its(:stdout){should match /^enp0s9/}
+  end
+
+  describe command('firewall-cmd --list-interface --zone=dmz') do
+    its(:stdout){should match /^enp0s9/}
+  end
+
+  describe command('firewall-cmd --list-service --zone=dmz --permanent') do
+    its(:stdout){should match /^openvpn/}
+  end
+  describe command('firewall-cmd --list-service --zone=dmz') do
+    its(:stdout){should match /^openvpn/}
+  end
+
+  describe command('firewall-cmd --list-service --zone=dmz --permanent') do
+    its(:stdout){should_not match /.*ssh.*/}
+  end
+  describe command('firewall-cmd --list-service --zone=dmz') do
+    its(:stdout){should_not match /.*ssh.*/}
+  end
+
+end
